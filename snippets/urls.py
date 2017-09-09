3474f5df5006edfcd7c581c7835d438253247035
Paul Huang <paul_hzq@hotmail.com>
2017-09-08 21:52:16 -0500

    rewrite the snippetlist view in class-based view

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,7 +3,8 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
+    # url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
 
--- snippets/views.py
+++ snippets/views.py
@@ -8,8 +8,24 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class SnippetList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+
+    def get(self, request, format = None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many = True)
+        return Response(serializer.data)
+
+
 
 
 @api_view(['GET','POST'])
